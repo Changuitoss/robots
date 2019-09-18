7cacd16aa8a6f1948cc9e59389f9e7f0bf218a90
Changuitoss <46825009+Changuitoss@users.noreply.github.com>
2019-09-17 22:40:42 -0300

    Agregue State

--- src/CardList.js
+++ src/CardList.js
@@ -7,8 +7,12 @@ const CardList = ({ robots }) => {
 		  { 
 		  robots.map((user, i) => {
 		  	return (
-		  		<Cards key={i} name={robots[i].name} email={robots[i].email} id={robots[i].id}/>
-		  	);
+		  		<Cards 
+			  		key={i} 
+			  		name={robots[i].name} 
+			  		email={robots[i].email} 
+			  		id={robots[i].id}/>
+		  			);
 		 })
 	  }
 	  </div>
